82d38599fb3a52e427aea72aaeb8e0663c8606c5
ankit.podder <ankitpodder0211@gmail.com>
2023-04-14 20:54:23 +0530

    Update branch

---
+++ BurgerProject/app/src/main/java/com/example/burgerproject/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.burgerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.*
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import com.example.burgerproject.databinding.ActivityMainBinding
+import com.example.burgerproject.databinding.ToastScreenBinding
+import kotlinx.coroutines.*
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var view: View
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        view = binding.root
+        setContentView(view)
+        binding.ivChecf.visibility = GONE
+
+
+        var clickEvent: View.OnClickListener = OnClickListener {
+            var ans =
+                "Your recepie is : \n" + (view.findViewById<RadioButton>(binding.rgRec.checkedRadioButtonId).text as String) + "\n"
+            ans += if (binding.cbCheese.isChecked) "Cheese\n" else ""
+            ans += if (binding.cbOnion.isChecked) "Onion\n" else ""
+            ans += if (binding.cbSalad.isChecked) "Salad\n" else ""
+
+            GlobalScope.launch(Dispatchers.Main) {
+                binding.ivChecf.visibility = VISIBLE
+                binding.tvBurger.text = ""
+                Toast.makeText(view.context, "Cooking your Burger...", Toast.LENGTH_SHORT).show()
+                Log.d("MainActivity", "Current Thread is : ${Thread.currentThread().name}")
+                delay(2500L)
+                Log.d("MainActivity", "Current Thread is : ${Thread.currentThread().name}")
+                binding.tvBurger.text = ans
+                binding.ivChecf.visibility = GONE
+
+            }
+
+        }
+        binding.btnGiveMyBurger.setOnClickListener(clickEvent)
+    }
+}
