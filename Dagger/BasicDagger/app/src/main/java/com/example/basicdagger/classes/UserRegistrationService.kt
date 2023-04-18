b8e65eb5c6cd75ea0fa582268cc19b01bbfd60fe
ankit.podder <ankitpodder0211@gmail.com>
2023-04-18 23:17:18 +0530

    Do constructor injection manually

--- Dagger/BasicDagger/app/src/main/java/com/example/basicdagger/classes/UserRegistrationService.kt
+++ Dagger/BasicDagger/app/src/main/java/com/example/basicdagger/classes/UserRegistrationService.kt
@@ -2,18 +2,11 @@ package com.example.basicdagger.classes
 
 import android.util.Log
 
-class UserRegistrationService {
-    val userRepository = UserRepository()
-    init {
-        Log.d(this::class.java.toString(), "Initialising UserRepository")
-    }
-
-    val notificationService = NotificationService()
-    init {
-        Log.d(this::class.java.toString(), "Initialising NotificationService")
-    }
-
-    fun registerUser(userName:String, email: String){
+class UserRegistrationService(
+    private val userRepository: UserRepository,
+    private val notificationService: NotificationService,
+) {
+    fun registerUser(userName: String, email: String) {
         userRepository.saveUser(userName, email)
         notificationService.sendMail(email)
     }
