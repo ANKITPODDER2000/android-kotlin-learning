30fe50a424370a4dab5014ae46e202c5d2681db3
ankit.podder <ankitpodder0211@gmail.com>
2023-05-16 00:37:18 +0530

    example of memory leak

--- Handler/app/src/main/java/com/example/handler/MainActivity.kt
+++ Handler/app/src/main/java/com/example/handler/MainActivity.kt
@@ -4,25 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.TextView
 import com.example.handler.databinding.ActivityMainBinding
 import java.lang.Exception
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private var isStopped = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var isStopped = false
 
-        val thread = Thread{
-            while(true){
-                if(isStopped) return@Thread
-                Log.d("MainActivity", "It's my thread...")
-                Thread.sleep(1000)
-            }
-        }
+        var thread = getThread()
 
         binding.btnStart.setOnClickListener {
             try {
@@ -36,6 +31,24 @@ class MainActivity : AppCompatActivity() {
         binding.btnStop.setOnClickListener {
             isStopped = true
             binding.tvTitle.text = "Stop thread..."
+        }
+    }
+
+    /*Still we are not in the UI thread, but still we can change the UI, this is Perfect example of memory leakage...*/
+    private fun getThread(): Thread {
+        return Thread {
+            var i = 0
+            while (true) {
+                if (isStopped) return@Thread
+                Log.d(
+                    "MainActivity",
+                    "It's my thread..., Thread name : ${Thread.currentThread().name}"
+                )
+                Thread.sleep(1000)
+                i++
+                // binding.tvCount.text = "Count: $i"
+                findViewById<TextView>(R.id.tvCount).text = "Count: $i"
+            }
         }
     }
 }
